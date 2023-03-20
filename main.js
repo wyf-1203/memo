7fa2f0df97b679337708665828a8176c23ba3887
王云飞1203 <228093287@qq.com>
2023-03-20 21:22:52 +0800

    基本功能完善

--- main.js
+++ main.js
@@ -2,23 +2,29 @@ const path = require('path');
 const { app, Menu, Tray, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
 const filePath = app.isPackaged
-  ? path.resolve(process.cwd(), 'resources', 'userData', 'userData.json')
+  ? path.resolve(path.dirname(app.getPath('exe')), 'resources', 'userData', 'userData.json')
   : path.resolve(__dirname, 'public', 'userData', 'userData.json');
 const iconPath = app.isPackaged
   ? path.resolve(__dirname, 'dist', 'memo.ico')
   : path.resolve(__dirname, 'public', 'memo.ico');
+const configPath = app.isPackaged
+  ? path.resolve(path.dirname(app.getPath('exe')), 'resources', 'configSetting', 'config.json')
+  : path.resolve(__dirname, 'public', 'configSetting', 'config.json');
 
-// const iconPath = path.resolve(__dirname, 'dist', 'memo.ico');
-// const filePath =    process.cwd(),
-//   'resources',
-//   'userData',
-//   'userData.json'
-// );
+
 const WinState = require('electron-win-state').default;
 const winState = new WinState({
   defaultWidth: 400,
   defaultHeight: 350,
 });
+
+const additionalData = { myKey: 'myValue' }
+const gotTheLock = app.requestSingleInstanceLock(additionalData)
+
+let win = null
+let tray = null;
+let config = null
+
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -51,79 +57,139 @@ const createWindow = () => {
     win.loadURL('http://127.0.0.1:5173/');
   }
 
-  ipcMain.handle('overhead', (e, flag) => {
-    // console.log(flag);
-    win.setAlwaysOnTop(flag);
-    // win.setIgnoreMouseEvents(flag, { forward: true });
-  });
-
-  ipcMain.handle('Ignore', (e, flag) => {
-    // console.log(flag);
-    // win.setAlwaysOnTop(true);
-    win.setIgnoreMouseEvents(flag, { forward: flag });
-  });
-
-  ipcMain.handle('minimize', () => {
-    // console.log('minimize');
-    // win.minimize();
-    win.hide();
-  });
-  tray.on('click', () => {
-    win.show();
-  });
-
   win.on('ready-to-show', () => {
     // win.show();
     winState.manage(win);
   });
+  return win
 };
-let tray = null;
-app.whenReady().then(() => {
-  tray = new Tray(iconPath);
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: '开机自启动',
-      type: 'checkbox',
-      checked: true,
-      click: () => {
-        // 点击事件：切换自启动
-        // console.log(app.getLoginItemSettings().openAtLogin);
-        if (!app.isPackaged) {
-          app.setLoginItemSettings({
-            openAtLogin: !app.getLoginItemSettings().openAtLogin, //获取当前自启动状态
-            path: process.execPath,
-          });
-        } else {
-          app.setLoginItemSettings({
-            openAtLogin: !app.getLoginItemSettings().openAtLogin,
-          });
+
+
+
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.whenReady().then(async () => {
+    tray = new Tray(iconPath);
+    config = await loadConfig()
+    console.log(config.AutoRunstart);
+    app.setLoginItemSettings({
+      openAtLogin: config.AutoRunstart, //获取当前自启动状态
+      path: process.execPath,
+    });
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: '开机自启动',
+        type: 'checkbox',
+        checked: app.getLoginItemSettings().openAtLogin,
+        click: () => {
+          // 点击事件：切换自启动
+          // console.log(app.getLoginItemSettings().openAtLogin);
+          if (!app.isPackaged) {
+            app.setLoginItemSettings({
+              openAtLogin: !app.getLoginItemSettings().openAtLogin, //获取当前自启动状态
+              path: process.execPath,
+            });
+            config.AutoRunstart = app.getLoginItemSettings().openAtLogin
+          } else {
+            app.setLoginItemSettings({
+              openAtLogin: !app.getLoginItemSettings().openAtLogin,
+            });
+            config.AutoRunstart = app.getLoginItemSettings().openAtLogin
+
+          }
+          console.log('开机状态:', config.AutoRunstart);
+          writeConfig(config)
+        },
+      },
+      {
+        label: '预留1', type: 'checkbox', checked: config.item1, click: () => {
+          config.item1 = !config.item1
+          writeConfig(config)
+
         }
       },
-    },
-    { label: '预留1', type: 'checkbox' },
-    { label: '预留2', type: 'checkbox', checked: true },
-    {
-      label: '退出',
-      click: () => {
-        app.quit();
+      {
+        label: '预留2', type: 'checkbox', checked: config.item2, click: () => {
+          config.item2 = !config.item2
+          writeConfig(config)
+        }
       },
-    },
-  ]);
-  tray.setToolTip('桌面便签');
-  tray.setContextMenu(contextMenu);
-  // 开机自启动
-  app.setLoginItemSettings({
-    openAtLogin: !app.getLoginItemSettings().openAtLogin, //获取当前自启动状态
-    path: process.execPath,
+      {
+        label: '退出',
+        click: () => {
+          app.quit();
+        },
+      },
+    ]);
+
+    tray.setToolTip('桌面便签');
+    tray.setContextMenu(contextMenu);
+    tray.on('click', () => {
+      win.show();
+    });
+    // 开机自启动
+
+    win = createWindow();
+
+    // console.log(winState); 
+    // console.info(winState);
+    // console.dir(winState.store);
+    let timer = null
+    win.addListener('move', () => {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        console.log('move');
+        winState.saveState(win)
+      }, 1000);
+    })
+    win.addListener('resize', () => {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        console.log('resize');
+        winState.saveState(win)
+      }, 1000);
+    })
+    // setInterval(() => {
+    //   winState.saveState(win)
+    // }, 2000)
+
   });
-  createWindow();
+
+  app.on('second-instance', (event, commandLine, workingDirectory, additionalData) => {
+    //输入从第二个实例中接收到的数据
+    console.log(additionalData)
+    console.log(win);
+    //有人试图运行第二个实例，我们应该关注我们的窗口
+    if (win) {
+      if (win.isMinimized()) win.restore()
+      win.focus()
+      win.show()
+    }
+  })
+
+}
+
+ipcMain.handle('overhead', (e, flag) => {
+
+  win.setAlwaysOnTop(flag);
+  // win.setIgnoreMouseEvents(flag, { forward: true });
 });
-app.commandLine.appendSwitch('wm-window-animations-disabled');
+
+ipcMain.handle('Ignore', (e, flag) => {
+
+  win.setIgnoreMouseEvents(flag, { forward: flag });
+});
+
+ipcMain.handle('minimize', () => {
+  win.hide();
+});
 
 ipcMain.handle('loadData', async () => {
   const data = await fs.readFileSync(filePath, { encoding: 'utf-8' });
   // console.log(data);
-  // console.log(app.isPackaged);
+  // console.log(app.isPackaged);    
   return { data, filePath };
 });
 
@@ -135,3 +201,22 @@ ipcMain.handle('writeFile', async (e, arr) => {
   });
   console.log(arrs);
 });
+
+const loadConfig = async () => {
+  const data = await fs.readFileSync(configPath, { encoding: 'utf-8' });
+
+
+  return JSON.parse(data)
+}
+const writeConfig = async (data) => {
+  const config = JSON.stringify(data)
+  console.log(config);
+  await fs.writeFileSync(configPath, config, (err) => {
+    console.log(err);
+  });
+}
+
+app.commandLine.appendSwitch('wm-window-animations-disabled');
+
+
+
--- src/view/home.vue
+++ src/view/home.vue
@@ -1,184 +1,195 @@
 <script setup>
-import {
-  ref,
-  reactive,
-  watch,
-  computed,
-  onMounted,
-  onBeforeMount,
-  onUpdated,
-} from 'vue'
-import router from '../router/index'
-import useUserDataStore from '../store/userDataStore'
-import { storeToRefs } from 'pinia'
-const btn = ref('TODO')
-
-const radio1 = ref('TODO')
-const UserDataStore = useUserDataStore()
-const { userData } = storeToRefs(UserDataStore)
-const loadData = UserDataStore.loadData
-loadData()
-let overheadFlag = ref(false)
-const minimize = () => {
-  window.myApi.minimize()
-}
-let nowTime = ref('0:00:00')
-
-const computedTime = () => {
-  setInterval(() => {
-    let time = new Date()
-    nowTime.value = timestampToTime(time)
-  }, 500)
-}
-computedTime()
-
-const timestampToTime = (timestamp) => {
-  // 时间戳为10位需*1000，时间戳为13位不需乘1000
-  // var date = new Date(timestamp * 1000);
-  // var date = new Date(timestamp)
-  var date = timestamp
-  // var Y = date.getFullYear() + '-'
-  // var M =
-  //   (date.getMonth() + 1 < 10
-  //     ? '0' + (date.getMonth() + 1)
-  //     : date.getMonth() + 1) + '-'
-  // var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' '
-  var h = date.getHours() + ':'
-  var m =
-    date.getMinutes() < 10
-      ? '0' + date.getMinutes() + ':'
-      : date.getMinutes() + ':'
-  var s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
-  // return Y + M + D + h + m + s;
-  return h + m + s
-}
-
-const overhead = () => {
-  overheadFlag.value = !overheadFlag.value
-  if (overheadFlag.value) {
-    document.querySelector('body').style.backgroundColor =
-      'rgba(12, 12, 12, 0.405)'
-  } else {
-    document.querySelector('body').style.backgroundColor =
-      'rgba(12, 12, 12, 0.705)'
+  import {
+    ref,
+    reactive,
+    watch,
+    computed,
+    onMounted,
+    onBeforeMount,
+    onUpdated,
+  } from 'vue'
+  import router from '../router/index'
+  import useUserDataStore from '../store/userDataStore'
+  import { storeToRefs } from 'pinia'
+  const btn = ref('TODO')
+  const radio1 = ref('TODO')
+  const UserDataStore = useUserDataStore()
+  const { userData } = storeToRefs(UserDataStore)
+  const loadData = UserDataStore.loadData
+  loadData()
+  let overheadFlag = ref(false)
+  const minimize = () => {
+    window.myApi.minimize()
   }
-  window.myApi.overhead(overheadFlag.value)
-}
+  let nowTime = ref('0:00:00')
 
-const mouseenter = () => {
-  if (overheadFlag.value) {
-    window.myApi.Ignore(false)
+  const computedTime = () => {
+    setInterval(() => {
+      let time = new Date()
+      nowTime.value = timestampToTime(time)
+    }, 500)
   }
-}
-const mouseleave = () => {
-  if (overheadFlag.value) {
-    window.myApi.Ignore(true)
-  } else {
-    window.myApi.Ignore(false)
-  }
-}
+  computedTime()
 
-const clickHandle = (text) => {
-  btn.value = text
-  radio1.value = text
-}
-watch(radio1, (newValue, oldValue) => {
-  // console.log(newValue);
-  if (newValue == 'TODO') {
-    router.replace('/home/todo')
-  } else if (newValue == 'DONE') {
-    router.replace('/home/done')
+  const timestampToTime = (timestamp) => {
+    // 时间戳为10位需*1000，时间戳为13位不需乘1000
+    // var date = new Date(timestamp * 1000);
+    // var date = new Date(timestamp)
+    var date = timestamp
+    // var Y = date.getFullYear() + '-'
+    // var M =
+    //   (date.getMonth() + 1 < 10
+    //     ? '0' + (date.getMonth() + 1)
+    //     : date.getMonth() + 1) + '-'
+    // var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' '
+    var h = date.getHours() + ':'
+    var m =
+      date.getMinutes() < 10
+        ? '0' + date.getMinutes() + ':'
+        : date.getMinutes() + ':'
+    var s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+    // return Y + M + D + h + m + s;
+    return h + m + s
   }
-})
-// onBeforeMount(() => {
-//   computedTime()
-// })
+
+  const overhead = () => {
+    overheadFlag.value = !overheadFlag.value
+    if (overheadFlag.value) {
+      document.querySelector('body').style.backgroundColor =
+        'rgba(12, 12, 12, 0.405)'
+    } else {
+      document.querySelector('body').style.backgroundColor =
+        'rgba(12, 12, 12, 0.705)'
+    }
+    window.myApi.overhead(overheadFlag.value)
+  }
+  const dragend = () => {
+    console.log('dragend');
+  }
+  const mouseenter = () => {
+    if (overheadFlag.value) {
+      window.myApi.Ignore(false)
+    }
+  }
+  const mouseleave = () => {
+    if (overheadFlag.value) {
+      window.myApi.Ignore(true)
+    } else {
+      window.myApi.Ignore(false)
+    }
+  }
+
+  const clickHandle = (text) => {
+    btn.value = text
+    radio1.value = text
+  }
+  watch(radio1, (newValue, oldValue) => {
+    // console.log(newValue);
+    if (newValue == 'TODO') {
+      router.replace('/home/todo')
+    } else if (newValue == 'DONE') {
+      router.replace('/home/done')
+    }
+  })
+  onBeforeMount(() => {
+    console.log(window.screenX)
+    console.log(window.screenY)
+  })
+
+  window.addEventListener('resize', () => {
+    console.log(window.screenX);
+  });
 // onUpdated(() => {
-//   computedTime()
+//   computedTime() 
 // })
 </script>
 
-  
+
 
 <template>
   <div class="tab">
     <div class="drag">
-      <span>作者: 阿飞五五开</span>
-      <span>桌面便签</span>
-      <span>{{nowTime}}</span>
+      <!-- <div style="-webkit-app-region: no-drag; z-index: 999; position: relative;width: 100%;height: 100%"> -->
+      <span> 桌面便签</span>
+      <span style="color: rgba(136, 139, 143, 0.342);">author: 阿飞</span>
+      <span> {{nowTime}}</span>
+      <!-- </div> -->
+
     </div>
     <div class="flex">
       <div class="flex">
-        <button
-          :class="['noDarg',overheadFlag?'':'btn', btn == 'TODO' ? 'clicked' : '']"
-          @click="clickHandle('TODO')"
-        >Todo</button>
+        <button :class="['noDarg',overheadFlag?'':'btn', btn == 'TODO' ? 'clicked' : '']"
+          @click="clickHandle('TODO')">Todo</button>
 
         <span style="color: #fff; display: inline-block">||</span>
 
-        <button
-          :class="['noDarg',overheadFlag?'':'btn', btn == 'DONE' ? 'clicked' : '']"
-          @click="clickHandle('DONE')"
-        >Done</button>
+        <button :class="['noDarg',overheadFlag?'':'btn', btn == 'DONE' ? 'clicked' : '']"
+          @click="clickHandle('DONE')">Done</button>
       </div>
 
       <div class="flex" style="margin-top:15px;width:100px;color: rgb(136, 139, 143); ">
-        <button
-          style="fontSize:22px"
+        <button style="fontSize:22px"
           :class="['iconfont',overheadFlag?'icon-yincangbukejian':'btn icon-yincangbukejian']"
-          @click="minimize"
-        ></button>
-        <button
-          :class="['btn','iconfont',overheadFlag?'icon-suoding':'icon-jiesuo']"
-          @mouseenter="mouseenter"
-          @mouseleave="mouseleave"
-          @click="overhead"
-        ></button>
+          @click="minimize"></button>
+        <button :class="['btn','iconfont',overheadFlag?'icon-suoding':'icon-jiesuo']" @mouseenter="mouseenter"
+          @mouseleave="mouseleave" @click="overhead"></button>
       </div>
     </div>
   </div>
   <router-view></router-view>
 </template>
 <style scoped>
-.flex {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.tab {
-  /* user-select: none; */
-}
-.drag {
-  position: relative;
-  -webkit-app-region: drag;
-  background-color: rgba(132, 143, 142, 0.322);
-  color: azure;
-  display: flex;
-  justify-content: space-between;
-}
-span {
-  margin: 5px 10px;
-}
-.clicked {
-  width: 100px;
-  font-weight: 900;
-  font-size: 1.2em;
-  /* color: aliceblue; */
-  color: rgb(243, 243, 243);
-}
+  .flex {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-.btn {
-  /* font-size: 20px; */
-}
-button {
-  padding: 0;
-}
-.btn:hover {
-  color: rgb(255, 255, 255);
-  cursor: pointer;
-}
-.overhead:hover {
-  /* color: rgb(136, 139, 143); */
-  cursor: pointer;
-}
+  .tab {
+    /* user-select: none; */
+  }
+
+  .drag {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    position: relative;
+    -webkit-app-region: drag;
+    background-color: rgba(132, 143, 142, 0.322);
+    color: azure;
+    display: flex;
+    justify-content: space-between;
+  }
+
+
+
+  span {
+    margin: 5px 10px;
+  }
+
+  .clicked {
+    width: 100px;
+    font-weight: 900;
+    font-size: 1.2em;
+    /* color: aliceblue; */
+    color: rgb(243, 243, 243);
+  }
+
+  .btn {
+    /* font-size: 20px; */
+  }
+
+  button {
+    padding: 0;
+  }
+
+  .btn:hover {
+    color: rgb(255, 255, 255);
+    cursor: pointer;
+  }
+
+  .overhead:hover {
+    /* color: rgb(136, 139, 143); */
+    cursor: pointer;
+  }
 </style>
